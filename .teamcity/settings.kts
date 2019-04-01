9518f3948fcf342835bdb8dfa88b589845acb870
gerald.muecke@devcon5.ch <gerald.muecke@devcon5.ch@TeamCity>
2019-04-01 18:17:21 +0200

    TeamCity change in 'devcon5.io / vertx-patterns' project: Versioned settings configuration updated

---
+++ .teamcity/settings.kts
@@ -0,0 +1,68 @@
+import jetbrains.buildServer.configs.kotlin.v2018_2.*
+import jetbrains.buildServer.configs.kotlin.v2018_2.buildSteps.maven
+import jetbrains.buildServer.configs.kotlin.v2018_2.triggers.vcs
+
+/*
+The settings script is an entry point for defining a TeamCity
+project hierarchy. The script should contain a single call to the
+project() function with a Project instance or an init function as
+an argument.
+
+VcsRoots, BuildTypes, Templates, and subprojects can be
+registered inside the project using the vcsRoot(), buildType(),
+template(), and subProject() methods respectively.
+
+To debug settings scripts in command-line, run the
+
+    mvnDebug org.jetbrains.teamcity:teamcity-configs-maven-plugin:generate
+
+command and attach your debugger to the port 8000.
+
+To debug in IntelliJ Idea, open the 'Maven Projects' tool window (View
+-> Tool Windows -> Maven Projects), find the generate task node
+(Plugins -> teamcity-configs -> teamcity-configs:generate), the
+'Debug' option is available in the context menu for the task.
+*/
+
+version = "2018.2"
+
+project {
+
+    buildType(Build)
+}
+
+object Build : BuildType({
+    name = "Build"
+
+    params {
+        param("env.JAVA_TOOL_OPTIONS", "")
+    }
+
+    vcs {
+        root(DslContext.settingsRoot)
+    }
+
+    steps {
+        maven {
+            goals = "clean test"
+            mavenVersion = defaultProvidedVersion()
+            jdkHome = "/usr/lib/jvm/jdk-11/"
+            jvmArgs = "-XX:+UnlockExperimentalVMOptions -XX:+EnableJVMCI -XX:+UseJVMCICompiler"
+            param("org.jfrog.artifactory.selectedDeployableServer.defaultModuleVersionConfiguration", "GLOBAL")
+        }
+        step {
+            type = "sonar-plugin"
+            param("sonarProjectSources", "src/main/java")
+            param("target.jdk.home", "%env.JDK_18_x64%")
+            param("sonarProjectTests", "src/test/java")
+            param("sonarProjectModules", "vertx-actors,vertx-caching,vertx-codec,vertx-services")
+            param("sonarProjectBinaries", "target/classes")
+            param("sonarServer", "7ba1e2c4-b91a-4c29-bd83-7948b20ea366")
+        }
+    }
+
+    triggers {
+        vcs {
+        }
+    }
+})
