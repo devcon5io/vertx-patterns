aee373ea2c53ffd91fc6aa04cb11f69cef9960fb
gerald.muecke@devcon5.ch <gerald.muecke@devcon5.ch@TeamCity>
2019-04-06 10:42:28 +0200

    TeamCity change in 'devcon5.io / vertx-patterns' project: runners of 'Build vertx-patterns' build configuration were updated

--- .teamcity/patches/buildTypes/Build.kts
+++ .teamcity/patches/buildTypes/Build.kts
@@ -1,6 +1,8 @@
 package patches.buildTypes
 
 import jetbrains.buildServer.configs.kotlin.v2018_2.*
+import jetbrains.buildServer.configs.kotlin.v2018_2.buildSteps.MavenBuildStep
+import jetbrains.buildServer.configs.kotlin.v2018_2.buildSteps.maven
 import jetbrains.buildServer.configs.kotlin.v2018_2.ui.*
 
 /*
@@ -13,4 +15,37 @@ changeBuildType(RelativeId("Build")) {
         "Unexpected option value: artifactRules = $artifactRules"
     }
     artifactRules = "**/target/*.jar"
+
+    expectSteps {
+        maven {
+            goals = "clean install"
+            mavenVersion = defaultProvidedVersion()
+            jdkHome = "/usr/lib/jvm/jdk-11/"
+            jvmArgs = "-XX:+UnlockExperimentalVMOptions -XX:+EnableJVMCI -XX:+UseJVMCICompiler"
+            param("org.jfrog.artifactory.selectedDeployableServer.defaultModuleVersionConfiguration", "GLOBAL")
+            param("org.jfrog.artifactory.selectedDeployableServer.deployArtifacts", "true")
+            param("org.jfrog.artifactory.selectedDeployableServer.envVarsExcludePatterns", "*password*,*secret*")
+            param("org.jfrog.artifactory.selectedDeployableServer.publishBuildInfo", "true")
+            param("org.jfrog.artifactory.selectedDeployableServer.targetRepo", "libs-release-local")
+            param("org.jfrog.artifactory.selectedDeployableServer.targetSnapshotRepo", "libs-snapshot-local")
+            param("org.jfrog.artifactory.selectedDeployableServer.urlId", "0")
+        }
+        step {
+            type = "sonar-plugin"
+            param("sonarProjectBinaries", "target/classes")
+            param("sonarProjectModules", "vertx-actors,vertx-caching,vertx-codec,vertx-services")
+            param("sonarProjectSources", "src/main/java")
+            param("sonarProjectTests", "src/test/java")
+            param("sonarServer", "7ba1e2c4-b91a-4c29-bd83-7948b20ea366")
+            param("target.jdk.home", "%env.JDK_18_x64%")
+        }
+    }
+    steps {
+        update<MavenBuildStep>(0) {
+            param("org.jfrog.artifactory.selectedDeployableServer.gitReleaseBranchNamePrefix", "REL-BRANCH-")
+            param("org.jfrog.artifactory.selectedDeployableServer.enableReleaseManagement", "true")
+            param("org.jfrog.artifactory.selectedDeployableServer.vcsTagsBaseUrlOrName", "v")
+            param("org.jfrog.artifactory.selectedDeployableServer.deployIncludePatterns", "**/target/*.jar")
+        }
+    }
 }
