1cd56809535524c1ad793d71448bb308e521f421
Gerald Mücke <gerald.muecke@devcon5.ch>
2019-04-09 07:50:38 +0200

    cleans up module-info

--- vertx-caching/src/main/java/io/devcon5/vertx/caching/AsyncCache.java
+++ vertx-caching/src/main/java/io/devcon5/vertx/caching/AsyncCache.java
@@ -1,6 +1,6 @@
 package io.devcon5.vertx.caching;
 
-import static org.slf4j.LoggerFactory.getLogger;
+import static io.vertx.core.logging.LoggerFactory.getLogger;
 
 import java.time.Duration;
 import java.util.ArrayList;
@@ -12,129 +12,144 @@ import java.util.function.Supplier;
 import io.vertx.core.AsyncResult;
 import io.vertx.core.Future;
 import io.vertx.core.Handler;
-import org.slf4j.Logger;
+import io.vertx.core.logging.Logger;
 
+/**
+ * Experimental asynchronous caching for vertx
+ * @param <T>
+ */
 public class AsyncCache<T> {
 
-   private final Map<String, CacheEntry<T>> issueLRUCache;
+  private final Map<String, CacheEntry<T>> issueLRUCache;
 
-   private Duration maxAge = Duration.ofSeconds(5);
+  private Duration maxAge = Duration.ofSeconds(5);
 
-   public AsyncCache<T> withMaxAge(Duration maxAge){
-      this.maxAge = maxAge;
+  public AsyncCache(int size) {
+
+    this.issueLRUCache = new LinkedHashMap<>(size, 0.75f, true);
+  }
+
+  public AsyncCache<T> withMaxAge(Duration maxAge) {
+
+    this.maxAge = maxAge;
+    return this;
+  }
+
+  /**
+   * Cache Entry to keep track of already requested issues. The cache entry ensures, no entry is older than 5s. An
+   * update policy can be defined, to update the value when the entry is expired. If nothing is done, the entry will
+   * expire causing read accesses to fail. The Entry holds a future that separates the registering of the entry ("the
+   * entry was requested") from the actual reading of the entry ("the entry was fetched"). This ensures, the entry is
+   * only fetched once (in 5s), even if additional requests for the same entry arrive before the entry is actually
+   * fetched.
+   *
+   * @param <T>
+   */
+  public static class CacheEntry<T> {
+
+    private static final Logger LOG = getLogger(CacheEntry.class);
+    private static final long DEFAULT_MAX_AGE = Duration.ofSeconds(5).toMillis();
+    private final String key;
+    private final List<Handler<AsyncResult<T>>> listeners = new ArrayList<>();
+    private long maxAge = DEFAULT_MAX_AGE;
+    private long expires;
+    private Future<T> value;
+    private Supplier<Future<T>> updater = expireEntryPolicy();
+    private CacheEntry(String key, final Future<T> value) {
+
+      this.key = key;
+      updateValue(value);
+    }
+
+    /**
+     * Default policy that expires the entry
+     */
+    public static final <T> Supplier<Future<T>> expireEntryPolicy() {
+
+      return () -> Future.failedFuture("Cache Entry expired");
+    }
+
+    public static <T> CacheEntry<T> createWithAutoUpdate(String key, Supplier<Future<T>> valueProvider) {
+
+      return new CacheEntry<>(key, valueProvider.get()).onExpiration(valueProvider);
+    }
+
+    public static <T> CacheEntry<T> create(String key, Future<T> valueProvider) {
+
+      return new CacheEntry<>(key, valueProvider);
+    }
+
+    /**
+     * Async read of the value.
+     *
+     * @param valueHandler
+     *     will be called, when the cached value is available.
+     *
+     * @return
+     */
+    public CacheEntry<T> readValue(Handler<AsyncResult<T>> valueHandler) {
+
+      if (isExpired()) {
+        updateValue(updater.get());
+      }
+
+      if (this.value.isComplete()) {
+        LOG.trace("Cache hit: {}", key);
+        valueHandler.handle(this.value);
+      } else {
+        this.listeners.add(valueHandler);
+      }
       return this;
-   }
+    }
 
-   public AsyncCache(int size){
-      this.issueLRUCache = new LinkedHashMap<>(size, 0.75f, true);
-   }
+    /**
+     * Defines an update policy that is used, when the cache entry expires.
+     *
+     * @param updatedValueProvider
+     *     provider for an updated version of the entry
+     *
+     * @return
+     */
+    public CacheEntry<T> onExpiration(Supplier<Future<T>> updatedValueProvider) {
 
-   /**
-    * Cache Entry to keep track of already requested issues. The cache entry ensures, no entry is older than 5s. An
-    * update policy can be defined, to update the value when the entry is expired. If nothing is done, the entry will
-    * expire causing read accesses to fail. The Entry holds a future that separates the registering of the entry ("the
-    * entry was requested") from the actual reading of the entry ("the entry was fetched"). This ensures, the entry is
-    * only fetched once (in 5s), even if additional requests for the same entry arrive before the entry is actually
-    * fetched.
-    *
-    * @param <T>
-    */
-   public static class CacheEntry<T> {
+      this.updater = updatedValueProvider;
+      return this;
+    }
 
-      private static final Logger LOGGER = getLogger(CacheEntry.class);
+    /**
+     * Defines the maximum age of the cache entry before it expires. When the value expires following read operation
+     * will either fail or execute any action that is defined as update policy. See onExpiration
+     *
+     * @param maxAge
+     *     the maximum age for the object
+     *
+     * @return this entry
+     */
+    public CacheEntry<T> maxAge(Duration maxAge) {
 
-      /**
-       * Default policy that expires the entry
-       */
-      public static final <T> Supplier<Future<T>> expireEntryPolicy() {
-         return () -> Future.failedFuture("Cache Entry expired");
-      }
+      this.maxAge = maxAge.toMillis();
+      return this;
+    }
 
-      private static final long DEFAULT_MAX_AGE = Duration.ofSeconds(5).toMillis();
+    private boolean isExpired() {
 
-      private final String key;
-      private long maxAge = DEFAULT_MAX_AGE;
-      private long expires;
-      private Future<T> value;
-      private Supplier<Future<T>> updater = expireEntryPolicy();
-      private final List<Handler<AsyncResult<T>>> listeners = new ArrayList<>();
+      return System.currentTimeMillis() > expires;
+    }
 
-      public static <T> CacheEntry<T> createWithAutoUpdate(String key, Supplier<Future<T>> valueProvider) {
-         return new CacheEntry<>(key, valueProvider.get()).onExpiration(valueProvider);
-      }
+    private void updateValue(Future<T> newValueFuture) {
 
-      public static <T> CacheEntry<T> create(String key, Future<T> valueProvider) {
-         return new CacheEntry<>(key, valueProvider);
-      }
-
-      private CacheEntry(String key, final Future<T> value) {
-         this.key = key;
-         updateValue(value);
-      }
-
-      /**
-       * Async read of the value.
-       *
-       * @param valueHandler will be called, when the cached value is available.
-       *
-       * @return
-       */
-      public CacheEntry<T> readValue(Handler<AsyncResult<T>> valueHandler) {
-         if (isExpired()) {
-            updateValue(updater.get());
-         }
-
-         if (this.value.isComplete()) {
-            LOGGER.trace("Cache hit: {}", key);
-            valueHandler.handle(this.value);
-         } else {
-            this.listeners.add(valueHandler);
-         }
-         return this;
-      }
-
-      /**
-       * Defines an update policy that is used, when the cache entry expires.
-       *
-       * @param updatedValueProvider provider for an updated version of the entry
-       *
-       * @return
-       */
-      public CacheEntry<T> onExpiration(Supplier<Future<T>> updatedValueProvider) {
-         this.updater = updatedValueProvider;
-         return this;
-      }
-
-      /**
-       * Defines the maximum age of the cache entry before it expires. When the value expires following read operation
-       * will either fail or execute any action that is defined as update policy. See onExpiration
-       *
-       * @param maxAge the maximum age for the object
-       *
-       * @return this entry
-       */
-      public CacheEntry<T> maxAge(Duration maxAge) {
-         this.maxAge = maxAge.toMillis();
-         return this;
-      }
-
-      private boolean isExpired() {
-         return System.currentTimeMillis() > expires;
-      }
-
-      private void updateValue(Future<T> newValueFuture) {
-         this.expires = System.currentTimeMillis() + maxAge;
-         this.value = newValueFuture;
-         //the handler ensures that all registered handler receive the new value once the future is completed.
-         //the handler list is cleared afterwards to not retain references when they are no longer needed.
-         //once the future is completed, any consecutive read operations will NOT add a new listener, but
-         //will forward the value directly (see readValue)
-         this.value.setHandler(fetched -> {
-            LOGGER.trace("Cache hit (lazy): {}", key);
-            listeners.forEach(l -> l.handle(this.value));
-            listeners.clear();
-         });
-      }
-   }
+      this.expires = System.currentTimeMillis() + maxAge;
+      this.value = newValueFuture;
+      //the handler ensures that all registered handler receive the new value once the future is completed.
+      //the handler list is cleared afterwards to not retain references when they are no longer needed.
+      //once the future is completed, any consecutive read operations will NOT add a new listener, but
+      //will forward the value directly (see readValue)
+      this.value.setHandler(fetched -> {
+        LOG.trace("Cache hit (lazy): {}", key);
+        listeners.forEach(l -> l.handle(this.value));
+        listeners.clear();
+      });
+    }
+  }
 
 }
