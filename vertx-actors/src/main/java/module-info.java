32ffc36321a8a30f754d67b484b3f6201a40e84b
Gerald Mücke <gerald.muecke@devcon5.ch>
2019-04-09 07:47:17 +0200

    cleans up module-info

--- vertx-actors/src/main/java/module-info.java
+++ vertx-actors/src/main/java/module-info.java
@@ -3,7 +3,5 @@
  */
 module io.devcon5.vertx.actors {
   exports io.devcon5.vertx.actors;
-  requires slf4j.api;
-  requires vertx.core;
   requires io.devcon5.vertx.codec;
 }
--- vertx-caching/src/main/java/module-info.java
+++ vertx-caching/src/main/java/module-info.java
@@ -3,5 +3,4 @@
  */
 module io.devcon5.vertx.caching {
    requires vertx.core;
-   requires slf4j.api;
 }
